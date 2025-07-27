e58eb1b227c1529bad117b7de84a8130920510e3
adarbayev <aa.darbayev@gmail.com>
2025-07-27 13:40:09 +0100

    Replace GRI, CDP, CSRD buttons with single EBRD button in navy blue

--- app/src/pages/ReportingPage.js
+++ app/src/pages/ReportingPage.js
@@ -10,17 +10,9 @@ const ReportingPage = () => {
     <div className="page-container reporting-modern">
       <h1 className="page-title">Reporting Workspace</h1>
       <div className="reporting-buttons-row">
-        <button className="reporting-big-btn cdp" onClick={() => handleReportButtonClick('CDP')}>
-          <span className="reporting-btn-icon">CDP</span>
-          <span className="reporting-btn-label">CDP Report</span>
-        </button>
-        <button className="reporting-big-btn gri" onClick={() => handleReportButtonClick('GRI')}>
-          <span className="reporting-btn-icon">GRI</span>
-          <span className="reporting-btn-label">GRI Report</span>
-        </button>
-        <button className="reporting-big-btn csrd" onClick={() => handleReportButtonClick('CSRD')}>
-          <span className="reporting-btn-icon">CSRD</span>
-          <span className="reporting-btn-label">CSRD/ESRS Report</span>
+        <button className="reporting-big-btn ebrd" onClick={() => handleReportButtonClick('EBRD')}>
+          <span className="reporting-btn-icon">EBRD</span>
+          <span className="reporting-btn-label">EBRD Report</span>
         </button>
       </div>
       <p className="reporting-disclaimer">
